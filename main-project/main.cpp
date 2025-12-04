e0517ae25a6ea95ec98e1ebd5ddf7b6866cfae59
kbroo <160418774+kbroo@users.noreply.github.com>
2025-12-04 16:50:22 +0300

    Добавлен вывод общей информации о задании

---
+++ main-project/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Roza Vetrov" << endl;
+	cout << "Variant: 6" << endl;
+	cout << "Author: Kvit Ilya" << endl;
+
+	return 0;
+
+}
